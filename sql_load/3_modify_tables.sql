3ba46d235793596f5f61493e3f2623832fc5c31f
clemsonexcel <113640553+clemsonexcel@users.noreply.github.com>
2025-03-01 15:13:57 +0100

    Update 3_modify_tables.sql

--- sql_load/3_modify_tables.sql
+++ sql_load/3_modify_tables.sql
@@ -24,28 +24,28 @@
  */
 -- NOTE: This has been updated from the video to fix issues with encoding
 COPY company_dim
-FROM 'C:\Users\hp mt44\Documents\BecomeExpert\SQL\csv_files\company_dim.csv' WITH (
+FROM '[file_path]' WITH (
         FORMAT csv,
         HEADER true,
         DELIMITER ',',
         ENCODING 'UTF8'
     );
 COPY skills_dim
-FROM 'C:\Users\hp mt44\Documents\BecomeExpert\SQL\csv_files\skills_dim.csv' WITH (
+FROM '[file_path]' WITH (
         FORMAT csv,
         HEADER true,
         DELIMITER ',',
         ENCODING 'UTF8'
     );
 COPY job_postings_fact
-FROM 'C:\Users\hp mt44\Documents\BecomeExpert\SQL\csv_files\job_postings_fact.csv' WITH (
+FROM '[file_path]' WITH (
         FORMAT csv,
         HEADER true,
         DELIMITER ',',
         ENCODING 'UTF8'
     );
 COPY skills_job_dim
-FROM 'C:\Users\hp mt44\Documents\BecomeExpert\SQL\csv_files\skills_job_dim.csv' WITH (
+FROM '[file_path]' WITH (
         FORMAT csv,
         HEADER true,
         DELIMITER ',',
